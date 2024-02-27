38123d40be0998f7538c191b6033f2629751d24e
David Bosnic <davidbosnic99@gmail.com>
2024-02-27 17:44:14 +0100

    Init commit

--- lua/David/core/options.lua
+++ lua/David/core/options.lua
@@ -2,6 +2,24 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+
+vim.cmd [[function! IndentWithI()
+    if len(getline('.')) == 0
+        return "\"_cc"
+    else
+        return "i"
+    endif
+endfunction
+nnoremap <expr> i IndentWithI()]]
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -19,6 +37,7 @@ vim.opt.undodir = "undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
